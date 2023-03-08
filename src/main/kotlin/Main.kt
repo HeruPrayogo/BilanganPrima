0565a8a0926c65e9f5755e698b0c7602fb553201
heru prayogo <heruprayogo062@gmail.com>
2023-03-08 19:54:55 +0700

    ulang

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,27 @@
-fun main() {
-    print("masukkan angka: ")
-    val a = readln()!!.toInt()
-        if(a % 2 == 1 || a / a == 1 ) {
-            println("$a adalah bilangan prima")
-        }else if(a / 3..9 == 2..9){
-            println("$a bukan bilangan prima")
+fun cek(number: Int): Boolean {
+        if (number <= 1) {
+            return false
         }
+
+        for (i in 2..number / 2) {
+            if (number % i == 0) {
+                return false
+            }
+        }
+
+        return true
+
+    }
+
+fun main() {
+    println("masukkan angka: ")
+    val number = readln()!!.toInt()
+
+    if (cek(number)) {
+        println("$number adalah bilangan prima")
+    } else {
+        println("$number bukan bilangan prima")
+    }
 }
+
+
