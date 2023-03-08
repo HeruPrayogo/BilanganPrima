c3105c78d2ba8fc1c424203745d8417761a98360
heru prayogo <heruprayogo062@gmail.com>
2023-03-08 19:32:29 +0700

    ulang

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
     print("masukkan angka: ")
     val a = readln()!!.toInt()
-        if(a % 2 == 1 && a / a == 1){
+        if(a % 2 == 1 && a / a == 1 && a / 3..9 == 2..9 ) {
             println("$a adalah bilangan prima")
         }else{
             println("$a bukan bilangan prima")
